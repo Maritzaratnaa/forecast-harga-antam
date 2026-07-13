8834a0f66b7550ee3cd93637d21fa0c02291455f
Maritzaratnaa <maritzaratnaa@gmail.com>
2026-07-13 10:40:26 +0700

    debug

--- etl/validation.py
+++ etl/validation.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 
 def validate(df: pd.DataFrame):
+    
+    print("Kolom yang tersedia saat ini:", df.columns.tolist())
 
     if df.isnull().sum().sum() > 0:
         print("Ada data NULL")
